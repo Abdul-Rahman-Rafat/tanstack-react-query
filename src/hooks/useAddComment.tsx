eb4bd9a3cb6f7682289de2a5d3f2d8774211a38f
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-06-17 16:38:01 +0300

    Optimistic update (add comments)

--- src/hooks/useAddComment.tsx
+++ src/hooks/useAddComment.tsx
@@ -1,34 +1,88 @@
-import { useMutation , UseMutationResult } from "@tanstack/react-query";
-import axios,{AxiosError} from "axios";
-
-interface CommentPost{
+import {
+    useMutation,
+    useQueryClient,
+  } from "@tanstack/react-query";
+  import axios, { AxiosError } from "axios";
+  
+  interface CommentPost {
     post_id: number;
     comment: string;
-}
-interface CommentResponse{
+  }
+  
+  interface CommentResponse {
     id: number;
     comment: string;
     post_id: number;
-}
-
-const requestData =async (data: CommentPost): Promise<CommentResponse> => {
+  }
+  
+  const requestData = async (
+    data: CommentPost
+  ): Promise<CommentResponse> => {
     const result = await axios.post<CommentResponse>(
-        `http://localhost:3000/comments`,
-        data
+      "http://localhost:3000/comments",
+      data
     );
+  
     return result.data;
-   }
-
-export default  function useAddComment(){
-    
-    return useMutation<CommentResponse, AxiosError, CommentPost>({
-        mutationFn: requestData,
-        onSuccess: (data) => {
-            console.log("Comment added successfully", data);
-        },
-        onError: (error) => {
-            console.log("Error adding comment", error);
-        },
+  };
+  
+  export default function useAddComment() {
+    const queryClient = useQueryClient();
+  
+    return useMutation<
+      CommentResponse,
+      AxiosError,
+      CommentPost,
+      { previousComments?: CommentResponse[] }
+    >({
+      mutationFn: requestData,
+  
+      // Optimistic Update
+      onMutate: async (newComment) => {
+        await queryClient.cancelQueries({
+          queryKey: [
+            "comments",
+            { post_id: newComment.post_id },
+          ],
+        });
+  
+        const previousComments =
+          queryClient.getQueryData<CommentResponse[]>([
+            "comments",
+            { post_id: newComment.post_id },
+          ]);
+  
+        queryClient.setQueryData<CommentResponse[]>(
+          ["comments", { post_id: newComment.post_id }],
+          (old = []) => [
+            ...old,
+            {
+              id: Date.now(), // temporary id
+              comment: newComment.comment,
+              post_id: newComment.post_id,
+            },
+          ]
+        );
+  
+        return { previousComments };
+      },
+  
+      // Rollback
+      onError: (_error, variables, context) => {
+        queryClient.setQueryData(
+          ["comments", { post_id: variables.post_id }],
+          context?.previousComments
+        );
+      },
+  
+      // Sync with server
+      onSettled: (_data, _error, variables) => {
+        queryClient.invalidateQueries({
+          queryKey: [
+            "comments",
+            { post_id: variables.post_id },
+          ],
+        });
+      },
     });
-
- }
+  }
