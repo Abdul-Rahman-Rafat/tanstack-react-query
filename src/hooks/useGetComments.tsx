2e698501ba780b7da99d5feca6f15fe2f4a34cb9
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-06-17 20:47:13 +0300

    abort request of comments (abort signal)

--- src/hooks/useGetComments.tsx
+++ src/hooks/useGetComments.tsx
@@ -1,16 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { CommentResponse } from "../Types";
+
+
+const getComments = async (id: number,signal:AbortSignal): Promise<CommentResponse[]> => {
+
+
+    const response = await axios.get<CommentResponse[]>(
+        `http://localhost:3000/comments?post_id=${+id}` , {signal}
+    );
+    
+    return response.data;
+}
+
 export default function useGetComments(id: string){
     return useQuery({
         queryKey: ["comments", { post_id: +id }],
-        queryFn: () => getComments(+id),
+        queryFn: ({signal}) => getComments(+id,signal),
     });
-}
-
-const getComments = async (id: number): Promise<CommentResponse[]> => {
-    const response = await axios.get<CommentResponse[]>(
-        `http://localhost:3000/comments?post_id=${+id}`
-    );
-    return response.data;
 }
